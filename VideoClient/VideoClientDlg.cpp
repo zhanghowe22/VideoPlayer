3fcdf0c66a236665880a21fc8262cb045c3c7ce3
zhanghowe <zhanghowe122@gmail.com>
2025-01-21 17:08:34 +0800

    1. 初步完成UI主要逻辑和代码

--- VideoClient/VideoClientDlg.cpp
+++ VideoClient/VideoClientDlg.cpp
@@ -21,11 +21,17 @@ CVideoClientDlg::CVideoClientDlg(CWnd* pParent /*=nullptr*/)
 	: CDialogEx(IDD_VIDEOCLIENT_DIALOG, pParent)
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+	m_status = false;
 }
 
 void CVideoClientDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDIT_PLAY, m_video);
+	DDX_Control(pDX, IDC_SLIDER_POS, m_pos);
+	DDX_Control(pDX, IDC_SLIDER_VOLUME, m_volume);
+	DDX_Control(pDX, IDC_EDIT_URL, m_url);
+	DDX_Control(pDX, IDC_BTN_PLAY, m_btnPlay);
 }
 
 BEGIN_MESSAGE_MAP(CVideoClientDlg, CDialogEx)
@@ -33,6 +39,12 @@ BEGIN_MESSAGE_MAP(CVideoClientDlg, CDialogEx)
 	ON_WM_QUERYDRAGICON()
 	ON_WM_TIMER()
 	ON_WM_DESTROY()
+	ON_BN_CLICKED(IDC_BTN_PLAY, &CVideoClientDlg::OnBnClickedBtnPlay)
+	ON_BN_CLICKED(IDC_BTN_STOP, &CVideoClientDlg::OnBnClickedBtnStop)
+	ON_NOTIFY(TRBN_THUMBPOSCHANGING, IDC_SLIDER_POS, &CVideoClientDlg::OnTRBNThumbPosChangingSliderPos)
+	ON_NOTIFY(TRBN_THUMBPOSCHANGING, IDC_SLIDER_VOLUME, &CVideoClientDlg::OnTRBNThumbPosChangingSliderVolume)
+	ON_WM_HSCROLL()
+	ON_WM_VSCROLL()
 END_MESSAGE_MAP()
 
 
@@ -48,6 +60,16 @@ BOOL CVideoClientDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
 	// TODO: 在此添加额外的初始化代码
+	SetTimer(0, 500, NULL);
+
+	m_pos.SetRange(0, 100);
+
+	m_volume.SetRange(0, 100);
+	m_volume.SetTic(10);
+	m_volume.SetTicFreq(20);
+	SetDlgItemText(IDC_STATIC_VOLUME, _T("100%"));
+
+	SetDlgItemText(IDC_STATIC_TIME, _T("--:--:--/--:--:--"));
 
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
@@ -93,6 +115,11 @@ HCURSOR CVideoClientDlg::OnQueryDragIcon()
 void CVideoClientDlg::OnTimer(UINT_PTR nIDEvent)
 {
 	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	if (nIDEvent == 0) {
+		// 控制层，获取播放状态，进度信息
+		// IDC_STATIC_VOLUME更新这个音量
+		// IDC_STATIC_TIME更新播放时间
+	}
 
 	CDialogEx::OnTimer(nIDEvent);
 }
@@ -101,6 +128,79 @@ void CVideoClientDlg::OnTimer(UINT_PTR nIDEvent)
 void CVideoClientDlg::OnDestroy()
 {
 	CDialogEx::OnDestroy();
+	KillTimer(0);
 
 	// TODO: 在此处添加消息处理程序代码
+}
+
+
+void CVideoClientDlg::OnBnClickedBtnPlay()
+{
+	
+	if (m_status == false) {
+		m_btnPlay.SetWindowText(_T("暂停"));
+		m_status = true;
+		// TODO: Controller的播放接口
+	}
+	else
+	{
+		m_btnPlay.SetWindowText(_T("播放"));
+		m_status = false;
+		// TODO: Controller的暂停接口
+	}
+}
+
+
+void CVideoClientDlg::OnBnClickedBtnStop()
+{
+	m_btnPlay.SetWindowText(_T("播放"));
+	m_status = false;
+	// TODO: 控制层的停止接口
+}
+
+
+void CVideoClientDlg::OnTRBNThumbPosChangingSliderPos(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	// 此功能要求 Windows Vista 或更高版本。
+	// _WIN32_WINNT 符号必须 >= 0x0600。
+	NMTRBTHUMBPOSCHANGING* pNMTPC = reinterpret_cast<NMTRBTHUMBPOSCHANGING*>(pNMHDR);
+	// TODO: 在此添加控件通知处理程序代码
+	TRACE("Pos: %d, reason: %d\r\n", pNMTPC->dwPos, pNMTPC->nReason);
+	*pResult = 0;
+}
+
+void CVideoClientDlg::OnTRBNThumbPosChangingSliderVolume(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	// 此功能要求 Windows Vista 或更高版本。
+	// _WIN32_WINNT 符号必须 >= 0x0600。
+	NMTRBTHUMBPOSCHANGING* pNMTPC = reinterpret_cast<NMTRBTHUMBPOSCHANGING*>(pNMHDR);
+	// TODO: 在此添加控件通知处理程序代码
+	TRACE("Pos: %d, reason: %d\r\n", pNMTPC->dwPos, pNMTPC->nReason);
+	*pResult = 0;
+}
+
+// 拖动视频进度条
+void CVideoClientDlg::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	TRACE("pos %p volume %p cur %p pos %d\r\n", &m_pos, &m_volume, pScrollBar, nPos);
+	if (nSBCode == 5) {
+		CString strTime;
+		strTime.Format(_T("%d%%"), nPos);
+		SetDlgItemText(IDC_STATIC_TIME, strTime);
+	}
+	CDialogEx::OnHScroll(nSBCode, nPos, pScrollBar);
+}
+
+// 拖动音量
+void CVideoClientDlg::OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	if (nSBCode == 5) {
+		TRACE("pos %p volume %p cur %p pos %d code %d\r\n", &m_pos, &m_volume, pScrollBar, nPos, nSBCode);
+		CString strVolume;
+		strVolume.Format(_T("%d%%"), 100 - nPos);
+		SetDlgItemText(IDC_STATIC_VOLUME, strVolume);
+	}
+	CDialogEx::OnVScroll(nSBCode, nPos, pScrollBar);
 }
