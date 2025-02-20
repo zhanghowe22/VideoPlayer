9e00d3ad986aea8f440536fc6ef8d6987b728477
zhanghowe <zhanghowe122@gmail.com>
2025-02-20 15:45:38 +0800

    1. 实现H264的解析

---
+++ VideoRTSPServer/MideaFile.cpp
@@ -0,0 +1,106 @@
+#include "MideaFile.h"
+
+MideaFile::MideaFile()
+	:m_file(NULL), m_type(-1)
+{
+}
+
+MideaFile::~MideaFile()
+{
+	Close();
+}
+
+int MideaFile::Open(const EBuffer& path, int nType)
+{
+	m_file = fopen(path, "rb");
+	if (m_file == NULL) {
+		// TODO: 错误提示
+		return -1;
+	}
+
+	m_type = nType;
+	// 获取文件大小
+	fseek(m_file, 0, SEEK_END);
+	m_size = ftell(m_file);
+	Reset(); // 读指针回到开头
+
+	return 0;
+}
+
+EBuffer MideaFile::ReadOneFrame()
+{
+	switch (m_type)
+	{
+	case 96:
+		return ReadH264Frame();
+	}
+	return EBuffer();
+}
+
+void MideaFile::Close()
+{
+	m_type = -1;
+	if (m_file != NULL) {
+		FILE* file = m_file;
+		m_file = NULL;
+		fclose(file);
+	}
+}
+
+void MideaFile::Reset()
+{
+	if (m_file) {
+		fseek(m_file, 0, SEEK_SET);
+	}
+}
+
+long MideaFile::FindH264Head()
+{
+	while (!feof(m_file))
+	{
+		char c = 0x7F;
+		while (!feof(m_file))
+		{
+			c = fgetc(m_file);
+			if (c == 0) break;
+		}
+		if (!feof(m_file)) {
+			c = fgetc(m_file);
+			if (c == 0) {
+				c = fgetc(m_file);
+				if (c == 1) { // 找到了一个头 0 0 1
+					return ftell(m_file) - 3;
+				}
+				else if (c == 0) {
+					c = fgetc(m_file);
+					if (c == 1) { // 找到了一个头 0 0 0 1
+						return ftell(m_file) - 4;
+					}
+				}
+			}
+		}
+	}
+	return -1;
+}
+
+EBuffer MideaFile::ReadH264Frame()
+{
+	if (m_file) {
+		long off = FindH264Head();
+		if (off == -1) return EBuffer();
+
+		fseek(m_file, off + 3, SEEK_SET); // 加3是为了减少再次find head的工作量，头最小长度为3
+		long tail = FindH264Head();
+
+		if (tail == -1) { // 第一次读已经是文件尾了
+			tail = m_size;
+		}
+		long size = tail - off;
+
+		fseek(m_file, off, SEEK_SET);
+		EBuffer result(size);
+		fread(result, 1, size, m_file);
+		return result;
+	}
+	return EBuffer();
+}
---
+++ VideoRTSPServer/MideaFile.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "base.h"
+
+class MideaFile
+{
+public:
+	MideaFile();
+
+	~MideaFile();
+
+	int Open(const EBuffer& path, int nType = 96);
+
+	// 如果buffer的size为0，则表示没有帧了
+	EBuffer ReadOneFrame();
+
+	void Close();
+
+	// 重置后，ReadOneFrame又会有值返回
+	void Reset();
+
+private:
+	// 返回-1表明查找失败
+	long FindH264Head();
+	EBuffer ReadH264Frame();
+
+private:
+	long m_size; // 文件大小
+	FILE* m_file;
+	EBuffer m_filePath;
+	// 96 H264
+	int m_type;
+};
+
